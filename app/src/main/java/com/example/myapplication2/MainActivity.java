cbd80f2ba01c92f51505e67188a76e87707f0ce3
pcloud zaman <pcloudzaman@yandex.com>
2019-06-23 14:57:01 -0700

    first dynamic fragment

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -1,11 +1,23 @@
 package com.example.myapplication2;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addFragmnet();
+
+    }
+    private void addFragmnet(){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        BlankFragment01 blankFragment01 = new BlankFragment01();
+        fragmentTransaction.add(R.id.container_framLayout, blankFragment01);
+        fragmentTransaction.commit();
 
     }
 }
