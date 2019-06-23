3e70d217e40c527878cfdd0b9b4b4fa1d1ecad07
pcloud zaman <pcloudzaman@yandex.com>
2019-06-23 14:27:44 -0700

    first static fragment

---
+++ app/src/main/java/com/example/myapplication2/BlankFragment01.java
@@ -0,0 +1,31 @@
+package com.example.myapplication2;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BlankFragment01 extends Fragment {
+
+
+    public BlankFragment01() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank_fragment01, container, false);
+    }
+
+}
